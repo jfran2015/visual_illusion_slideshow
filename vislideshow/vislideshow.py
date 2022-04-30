e9193117f7f7fb29b88d52718124650d3834d490
Justin Frandsen <jfran4047@gmail.com>
2022-04-30 15:35:08 -0500

    (30/04/2022)

--- vislideshow/vislideshow.py
+++ vislideshow/vislideshow.py
@@ -1,7 +1,7 @@
-import tkinter as tk
-from tkinter import *
-from PIL import Image
-from PIL import ImageTk
+from pickle import FALSE, TRUE
+from turtle import width
+import pygame
+import os
 
 class format:
     num_format = 0
@@ -9,7 +9,7 @@ class format:
     def __init__(self, name, path):
         self.name = name
         self.path = path
-        num_format += 1
+        format.num_format += 1
         
 
 class gif(format):
@@ -18,7 +18,7 @@ class gif(format):
     def __init__(self, name, path, extension):
         super().__init__(name, path)
         self.extension = extension
-        num_gif += 1
+        gif.num_gif += 1
         
 class video(format):
     num_video = 0
@@ -26,22 +26,58 @@ class video(format):
     def __init__(self, name, path, extension):
         super().__init__(name, path)
         self.extension = extension
-        num_video += 1
+        video.num_video += 1
         
 class image(format):
     num_image = 0
     
     def __init__(self, name, path, extension):
         super().__init__(name, path)
-        self.extension = extension
-        num_image += 1
+        self.extension = extension        
+        image.num_image += 1
+   
+#read in folder with illusions in it and sort to relative classes     
+def sort_illusions(folder):
+    paths = os.listdir(folder)
+    for file in paths:
+        print(file)
+        if file[-4:] == ".png" or file[-4:] == ".jpg":
+        
+  
         
 
 
 if __name__ == "__main__":
-   print("File one executed when ran directly")
-   kanizsaTriangle = image("Kanizsa Triangle", "files/35_kanizsa_main.png", ".png")
-   print(kanizsaTriangle.name)
-   
+    image1 = image("Kanizsa Triangle", r"\files\35_kanizsa_main.png", ".png")
+    
+    sort_illusions('files/')
+    
+    
+    image = pygame.image.load(r'files\rotsnake2.gif')
+    background_color = (255,255,255)
+    (width, height) = (500, 500)
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Visual Illusions")
+    screen.fill(background_color)
+    screen.blit(image, (0,0))
+    
+    
+    pygame.display.flip()
+    running = TRUE
+    while running:
+        
+        screen.fill(background_color)
+        screen.blit(image, (0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = FALSE
+                pygame.quit()
+                quit()  
+            pygame.display.update()
+
+
 else:
-   print("File one executed when imported")
+    print("file not rain as main")            
+   
+kanizsaTriangle = image("Kanizsa Triangle", "files/35_kanizsa_main.png", ".png")
+   
